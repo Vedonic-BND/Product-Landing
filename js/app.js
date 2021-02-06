34c5dd8445126d3ac8b0465aeefc188d1fdf480d
Vedonic-BND <vdn3903@students.uc-bcf.edu.ph>
2021-02-07 05:31:55 +0800

    Product Landing Website

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let menuToggler = document.querySelector(".nav_button");
+let navLinks = document.querySelectorAll(".nav-link");
+let body = document.querySelector("body");
+
+menuToggler.addEventListener("click", () => {
+	body.classList.toggle("open");
+});
+
+navLinks.forEach(link => {
+	link.addEventListener("click", () => {
+		body.classList.toggle("open");
+	});
+});
+
+
+//Animation on Scroll 
+AOS.init();
